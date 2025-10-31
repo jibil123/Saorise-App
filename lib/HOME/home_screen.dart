10337f4dc5d83b257fea973407c99e5ae63056a7
Jibil PT <jibilptjibil@gmail.com>
2025-10-31 16:52:12 +0530

    some bugs are commanded needs to fix those

--- lib/HOME/home_screen.dart
+++ lib/HOME/home_screen.dart
@@ -102,7 +102,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    getUserDetails();
+    // getUserDetails();
     fetchProducts();
     _updateSystemUIOverlay();
   }
